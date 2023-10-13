374ab361a68439f72611e130d59d29f19b9310f0
layasimon <146072706+layasimon@users.noreply.github.com>
2023-10-13 16:05:26 +0530

    Add files via upload

---
+++ dictionary7.py
@@ -0,0 +1,6 @@
+
+l=int(input("enter the number"))
+d={}
+for n in range(1,l+1):
+    d[n]=n**2
+print("\n dictionary=",d)
---
+++ numberlist.py
@@ -0,0 +1,10 @@
+start=int(input("enter the first number of your list:"))
+last=int(input("enterthe last number of your list:"))
+num=start
+for num in range(start,last+1,1):
+    print("your list is :",num)
+for num in range(start,last+1,1):
+       if(num>0):
+print("the list of positive number is",num) 
+
+
