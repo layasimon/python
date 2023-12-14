0a24768677a27ca744bf58c33e31e813400e8657
layasimon <146072706+layasimon@users.noreply.github.com>
2023-12-14 12:21:48 +0530

    Add files via upload

---
+++ package package/package/main.py
@@ -0,0 +1,9 @@
+from mypackage import summodule as sum1
+from mypackage import avgmodule as avarage1
+from mypackage import powermodule as power1
+a=sum1.sum(10,20)
+b=avarage1.avarage(10,20,30)
+c=power1.power(1,2)
+print(a)
+print(b)
+print(c)
---
+++ package package/package/mypackage/avgmodule.py
@@ -0,0 +1,2 @@
+def avarage(a,b,c):
+    return(a+b+c)/3
